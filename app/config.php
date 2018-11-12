6bb1240396ffb02c81e7ca6d2ffdb90f0a1e3f8b
Anton Kobzar <freshdv@mail.ru>
2018-11-12 13:59:40 +0500

    Update app/config.php
    token

--- app/config.php
+++ app/config.php
@@ -5,8 +5,8 @@
  */
 return [
 
-    'apiKey' => '',
-    'webhookUrl' => '',
+    'apiKey' => '48bc74e0a2a7d422-30bba09a32317b03-745e8a830952ebfc',
+    'webhookUrl' => 'https://ms11bot2.herokuapp.com/app/bot.php',
     'debug' => 0,
 ];
 
